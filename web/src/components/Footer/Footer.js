3d485185c1fc26aff3b64dccc09136dbbe8732ff
Free Wortley <jwortley@uber.com>
2015-08-08 00:19:55 -0700

    Initial Web UI Foundation

---
+++ web/src/components/Footer/Footer.js
@@ -0,0 +1,38 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { PropTypes } from 'react';
+import styles from './Footer.css';
+import withViewport from '../../decorators/withViewport';
+import withStyles from '../../decorators/withStyles';
+import Link from '../../utils/Link';
+
+@withViewport
+@withStyles(styles)
+class Footer {
+
+  static propTypes = {
+    viewport: PropTypes.shape({
+      width: PropTypes.number.isRequired,
+      height: PropTypes.number.isRequired
+    }).isRequired
+  };
+
+  render() {
+    // This is just an example how one can render CSS
+    let { width, height } = this.props.viewport;
+    this.renderCss(`.Footer-viewport:after {content:' ${width}x${height}';}`);
+
+    return (
+      <div className="Footer">
+        <div className="Footer-container">
+          <span className="Footer-text">© Let's Hack Things!</span>
+          <span className="Footer-spacer"> | </span>
+          <span ref="viewport" className="Footer-viewport Footer-text Footer-text--muted">Viewport:</span>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Footer;
---
+++ web/src/components/Navigation/Navigation.js
@@ -0,0 +1,25 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+import styles from './Navigation.css';
+import withStyles from '../../decorators/withStyles';
+import Link from '../../utils/Link';
+
+@withStyles(styles)
+class Navigation {
+
+  static propTypes = {
+    className: PropTypes.string
+  };
+
+  render() {
+    return (
+      <div className={classNames(this.props.className, 'Navigation')} role="navigation">
+      </div>
+    );
+  }
+
+}
+
+export default Navigation;
