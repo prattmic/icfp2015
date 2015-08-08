543d8eba08d7ff28984c91070e53b155e55a8824
Alex Ray <a@machinaut.com>
2015-08-08 15:36:15 -0700

    Hex size settings

--- render.go
+++ render.go
@@ -10,16 +10,19 @@ import (
 	"math"
 )
 
-const (
-	hexsize   = 5    // Hexagon size in pixels
-	hexhoriz  = 6    // Horizontal spacing of hexagons
-	hexvert   = 4    // Vertical spacing of hexagons
-	hexborder = 40   // Border pixels to edge of rectangle
-	hexpivot  = 2    // Pivot size in pixels
-	c_empty   = iota // keep this one first, we rely on it being 0
-	c_fill
-	c_unit
+const ( // Sizes of hexagons to render
+	tinySize  = 0
+	smallSize = 1
+	medSize   = 2
+	largeSize = 3
 )
+
+type HexSize struct {
+	size  int // Hexagon size (width and height) in pixels
+	horiz int // Horizontal spacing in pixels (incl. hexagon size)
+	vert  int // Vertical spacing in pixels (incl. hexagon size)
+	pivot int // Pivot size in pixels
+}
 
 var (
 	// Colors to draw with
@@ -35,10 +38,16 @@ var (
 	red    = image.Uniform{redColor}
 	grey   = image.Uniform{greyColor}
 	black  = image.Uniform{blackColor}
+	// Hexagon presize
+	tinyHex  = HexSize{size: 3, horiz: 4, vert: 3, pivot: 1}
+	smallHex = HexSize{size: 5, horiz: 6, vert: 4, pivot: 2}
+	medHex   = HexSize{size: 10, horiz: 12, vert: 9, pivot: 4}
+	largeHex = HexSize{size: 20, horiz: 24, vert: 18, pivot: 8}
 )
 
 type HexMask struct {
-	cell Cell // Game board coordinate
+	cell Cell    // Game board coordinate
+	hs   HexSize // Size to draw things
 }
 
 func (hm *HexMask) ColorModel() color.Model {
@@ -46,96 +55,60 @@ func (hm *HexMask) ColorModel() color.Model {
 }
 
 func (hm *HexMask) origin() image.Point {
-	px := hm.cell.X*hexhoriz + (hm.cell.Y%2)*hexhoriz/2
-	py := hm.cell.Y * hexvert
+	px := hm.cell.X*hm.hs.horiz + (hm.cell.Y%2)*hm.hs.horiz/2
+	py := hm.cell.Y * hm.hs.vert
 	return image.Point{px, py}
 }
 
 func (hm *HexMask) Bounds() image.Rectangle {
-	return image.Rect(0, 0, hexsize, hexsize)
+	return image.Rect(0, 0, hm.hs.size, hm.hs.size)
 }
 
 func (hm *HexMask) At(xx, yy int) color.Color {
 	x := float64(xx)
 	y := float64(yy)
-	k := float64(hexsize)
-	h := float64(hexsize-1) / 2.0
+	k := float64(hm.hs.size)
+	h := float64(hm.hs.size-1) / 2.0
 	if (k - 2*math.Abs(y-h)) <= math.Abs(x-h) {
 		return color.Alpha{0}
 	}
 	return color.Alpha{255}
 }
 
-func drawHex(m draw.Image, board image.Rectangle, cell Cell, i image.Image) {
-	hm := HexMask{cell}
+func drawHex(m draw.Image, board image.Rectangle, cell Cell, hs HexSize, i image.Image) {
+	hm := HexMask{cell, hs}
 	r := hm.Bounds().Add(board.Min).Add(hm.origin())
 	draw.DrawMask(m, r, i, image.ZP, &hm, image.ZP, draw.Over)
 }
 
-func drawPivot(m draw.Image, board image.Rectangle, cell Cell) {
-	h := (hexsize - hexpivot) / 2
-	x := cell.X*hexhoriz + (cell.Y%2)*hexhoriz/2 + h
-	y := cell.Y*hexvert + h
+func drawPivot(m draw.Image, board image.Rectangle, cell Cell, hs HexSize) {
+	h := (hs.size - hs.pivot) / 2
+	x := cell.X*hs.horiz + (cell.Y%2)*hs.horiz/2 + h
+	y := cell.Y*hs.vert + h
 	min := board.Min.Add(image.Point{x, y})
-	max := min.Add(image.Point{hexpivot, hexpivot})
+	max := min.Add(image.Point{hs.pivot, hs.pivot})
 	draw.Draw(m, image.Rectangle{min, max}, &black, image.ZP, draw.Over)
-}
-
-func fillColor(c int) image.Image {
-	switch c {
-	case c_fill:
-		return &yellow
-	case c_unit:
-		return &red
-	}
-	return &grey
-}
-
-func memoizeCells(fill, unit []Cell, width, height int) []int {
-	memoized := make([]int, width*height) // start out all c_empty
-	for _, cell := range fill {
-		memoized[cell.X*width+cell.Y] = c_fill
-	}
-	for _, cell := range unit {
-		memoized[cell.X*width+cell.Y] = c_unit
-	}
-	return memoized
-}
-
-// This takes an io.Writer, and renders a GIF of the InputProblem to it
-func RenderInputProblem(w io.Writer, ip *InputProblem, border int) {
-	height := ip.Height
-	width := ip.Width
-	memo := memoizeCells(ip.Filled, nil, width, height)
-
-	board := image.Rect(border, border, hexhoriz/2+hexhoriz*width, hexvert*height)
-	rect := image.Rectangle{image.ZP, board.Max.Add(image.Pt(border, border))}
-
-	// background
-	m := image.NewPaletted(rect, pal)
-	draw.Draw(m, rect, &white, image.ZP, draw.Src)
-
-	// TODO(aray): dont hardcode pixels based on 20px hexagons
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			c := memo[x*width+y]
-			drawHex(m, board, Cell{x, y}, fillColor(c))
-		}
-	}
-
-	// end of not a loop anymore
-	image := []*image.Paletted{m}
-	anim := gif.GIF{Image: image, Delay: []int{10}, LoopCount: 1}
-	gif.EncodeAll(w, &anim)
 }
 
 type GameRenderer struct {
 	width, height, border int
 	frames                []*image.Paletted
+	hs                    HexSize
 }
 
-func NewGameRenderer(g *Game, border int) *GameRenderer {
-	return &GameRenderer{width: g.b.width, height: g.b.height, border: border}
+func NewGameRenderer(g *Game, border int, size int) *GameRenderer {
+	hs := largeHex // Default
+	switch size {
+	case tinySize:
+		hs = tinyHex
+	case smallSize:
+		hs = smallHex
+	case medSize:
+		hs = medHex
+	case largeSize:
+		hs = largeHex
+	}
+	return &GameRenderer{width: g.b.width, height: g.b.height, border: border, hs: hs}
 }
 
 func gameFillColor(g *Game, x, y int) image.Image {
@@ -158,8 +131,9 @@ func (r *GameRenderer) AddFrame(g *Game) {
 	height := r.height
 	width := r.width
 	border := r.border
+	hs := r.hs
 
-	board := image.Rect(border, border, hexhoriz/2+hexhoriz*width, hexvert*height)
+	board := image.Rect(border, border, hs.horiz/2+hs.horiz*width, hs.vert*height)
 	rect := image.Rectangle{image.ZP, board.Max.Add(image.Pt(border, border))}
 
 	// background
@@ -168,10 +142,10 @@ func (r *GameRenderer) AddFrame(g *Game) {
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			drawHex(m, board, Cell{x, y}, gameFillColor(g, x, y))
+			drawHex(m, board, Cell{x, y}, hs, gameFillColor(g, x, y))
 		}
 	}
-	drawPivot(m, board, g.currUnit.Pivot)
+	drawPivot(m, board, g.currUnit.Pivot, hs)
 
 	r.frames = append(r.frames, m)
 }
