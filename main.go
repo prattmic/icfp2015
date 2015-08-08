7cf3abe9c215f58aee730415d9c1578a7b98a53e
Michael Pratt <michael@pratt.im>
2015-08-07 20:16:41 -0700

    Allow -p and -f to be specified multiple times
    Each time the value accumulates into a slice.

--- main.go
+++ main.go
@@ -7,17 +7,34 @@ import (
 )
 
 var (
-	inputFile = flag.String("f", "", "File containing JSON encoded input.")
+	// These are registered in init(), below.
+	inputFiles multiStringValue
+	powerPhrases multiStringValue
+
 	timeLimit = flag.Int("t", 1000, "Time limit, in seconds, to produce output.")
-	powerPhrase = flag.String("p", "", "Phrase of power")
 
 	// TODO(myenik) Lol we should think about how to deal with this one...
 	memLimit = flag.Int("m", 1000, "Memory limit, in megabytes, to produce output")
 )
 
+// multiStringValue is a flag.Value which can be specified multiple times
+// on the command line.
+type multiStringValue []string
+
+// String implements flag.Value.String.
+func (s *multiStringValue) String() string {
+	return fmt.Sprintf("%v", *s)
+}
+
+// String implements flag.Value.Set, adding each new item to the slice.
+func (s *multiStringValue) Set(v string) error {
+	*s = append(*s, v)
+	return nil
+}
+
 func ArgsOk() error {
-	if *inputFile == "" {
-		return fmt.Errorf("no file name specified")
+	if len(inputFiles) == 0 {
+		return fmt.Errorf("no file names specified")
 	}
 
 	return nil
@@ -32,17 +49,26 @@ func main() {
 		return
 	}
 
-	f, err := os.Open(*inputFile)
-	if err != nil {
-		fmt.Printf("Could not open input file %s: %v\n", *inputFile, err)
-		return
-	}
+	for _, name := range inputFiles {
+		fmt.Printf("Processing %s\n", name)
 
-	problem, err := ParseInputProblem(f)
-	if err != nil {
-		fmt.Printf("Could not parse JSON in input file %s: %v\n", *inputFile, err)
-		return
-	}
+		f, err := os.Open(name)
+		if err != nil {
+			fmt.Printf("Could not open input file %s: %v\n", name, err)
+			return
+		}
 
-	fmt.Printf("Dump of parsed input:\n%+v\n", problem)
+		problem, err := ParseInputProblem(f)
+		if err != nil {
+			fmt.Printf("Could not parse JSON in input file %s: %v\n", name, err)
+			return
+		}
+
+		fmt.Printf("Dump of parsed input:\n%+v\n", problem)
+	}
+}
+
+func init() {
+	flag.Var(&inputFiles, "f",  "File containing JSON encoded input.")
+	flag.Var(&powerPhrases, "p",  "Phrase of power")
 }
