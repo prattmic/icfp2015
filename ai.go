a45e4172aee525dd40ff625e89d53a19afe8694a
Michael Pratt <michael@pratt.im>
2015-08-08 17:44:09 -0700

    Basic one-step lookahead AI
    Perhaps has some thread-safety issues?

--- ai.go
+++ ai.go
@@ -46,22 +46,78 @@ func NewAI(g *Game) AI {
 	return AI{Game: g}
 }
 
-// Next steps the game, returning true when the game is done.
+type aiResult struct {
+	command Command
+	game *Game
+	score float64
+	done bool
+	err error
+}
+
 func (a *AI) Next() (bool, error) {
-	moves := NewMovePermuter()
+	var coms Commands
+	for _, d := range []Direction{E, W, SE, SW, CCW, CW} {
+		coms = append(coms, directionToCommands[d][0])
+	}
 
-	// Keep trying moves until one works.
-	for {
-		c, err := moves.Next()
-		if err != nil {
-			// No possible moves, we are stuck!
-			return false, err
-		}
+	// Try each command, use the highest score.
+	ch := make(chan aiResult, len(coms))
 
-		done, err := a.Game.Update(c)
-		log.Printf("Update(%s) -> %v, %v", c, done, err)
-		if err == nil {
-			return done, nil
+	runner := func(c Command) {
+		g := a.Game.Fork()
+		done, err := g.Update(c)
+
+		ch <- aiResult{
+			command: c,
+			game: g,
+			score: g.Score(),
+			done: done,
+			err: err,
 		}
 	}
+
+	// Compute moves
+	for _, c := range coms {
+		go runner(c)
+	}
+
+	// Collect results
+	var ret []aiResult
+	for range coms {
+		ret = append(ret, <-ch)
+	}
+
+	bestScore := -1.0
+	var best aiResult
+	for _, r := range ret {
+		if r.err != nil {
+			log.Printf("skipping: %+v", r)
+			continue
+		}
+
+		if r.score > bestScore {
+			log.Printf("best by score: %+v", r)
+			bestScore = r.score
+			best = r
+		}
+
+		if best.done && !r.done {
+			log.Printf("best by not done: %+v", r)
+			bestScore = r.score
+			best = r
+		}
+	}
+
+	log.Printf("results: %+v", ret)
+
+	if best.game == nil {
+		// No best move! Just use first.
+		best = ret[0]
+		log.Printf("best by best is nil: %+v", best)
+	}
+
+	log.Printf("best: %+v", best)
+
+	a.Game = best.game
+	return best.done, best.err
 }
