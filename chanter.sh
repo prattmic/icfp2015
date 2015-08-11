0efeeb8ee626d816a74802058cb7d7473d615c8c
Alex Ray <aray@planet.com>
2015-08-11 11:14:59 -0700

    full run results

--- chanter.sh
+++ chanter.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
+touch fullrun.txt
 for i in `seq 0 24`;
 do
     #echo "./play_icfp2015 -ai chanterai -f qualifiers/problem_${i}.json"
-    ./play_icfp2015 -ai chanterai -f qualifiers/problem_${i}.json | ./submit.sh
+    ./play_icfp2015 -ai chanterai -f qualifiers/problem_${i}.json >> \
+        fullrun.txt
 done
